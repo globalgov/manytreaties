f08800253d3df05ad52e8d7a7281fc1659def516
Jael Tan <jael.tan@graduateinstitute.ch>
2025-02-25 14:30:38 +0100

    Corrected issues with table

--- R/manytreaties-agreements.R
+++ R/manytreaties-agreements.R
@@ -41,13 +41,13 @@
 #' }
 #' \item{IEADB: }{
 #' Mitchell, Ron B. et al. 2020.
-#' What we know (and could know) about international environmental agreements.
+#' "What we know (and could know) about international environmental agreements".
 #' _Global Environmental Politics_ 20.1, pp. 103-121.
 #' \url{https://www.iea.ulaval.ca/en/agreements}
 #' }
 #' \item{HEIDI: }{
 #' Morin, Jean-Frederic, and Chantal Blouin. 2019.
-#' How environmental treaties contribute to global health governance.
+#' "How environmental treaties contribute to global health governance".
 #' _Globalization and health_ 15.1, pp. 1-8.
 #' \url{https://www.chaire-epi.ulaval.ca/en/data/heidi}
 #' }
@@ -58,25 +58,25 @@
 #' }
 #' \item{TREND: }{
 #' Morin, Jean-Frederic, Andreas Dür, and Lisa Lechner. 2018.
-#' Mapping the trade and environment nexus: Insights from a new dataset.
+#' "Mapping the trade and environment nexus: Insights from a new dataset".
 #' _Global Environmental Politics_ 18.1, pp. 122-139.
 #' \url{http://www.chaire-epi.ulaval.ca/en/trend}
 #' }
 #' \item{LABPTA: }{
 #' Raess, Damien, Andreas Dür, and D. Sari. 2018.
-#' Protecting labor rights in preferential trade agreements: The role of trade unions, left governments, and skilled labor.
+#' "Protecting labor rights in preferential trade agreements: The role of trade unions, left governments, and skilled labor".
 #' _The Review of International Organizations_ 2.13, pp. 143-162.
 #' \doi{10.1007/s11558-018-9301-z}
 #' }
 #' \item{TOTA: }{
 #' Alschner, Wolfgang, Julia Seiermann, and Dmitriy Skougarevskiy. 2017.
-#' Text-as-data analysis of preferential trade agreements: Mapping the PTA landscape.
+#' "Text-as-data analysis of preferential trade agreements: Mapping the PTA landscape".
 #' UNCTAD Research Paper No. 5.
 #' \url{https://github.com/mappingtreaties/tota.git}.
 #' }
 #' \item{GPTAD: }{
 #' World Bank Group. 2014.
-#' Global Preferential Trade Agreement Database (GPTAD).
+#' "Global Preferential Trade Agreement Database (GPTAD)".
 #' Online database. _World Bank Group_.
 #' \url{https://wits.worldbank.org/gptad/library.aspx}
 #' }
@@ -84,20 +84,20 @@
 #' @section Mapping:
 #' 
 #' |  *manytreaties*  | *IEADB*  | *HEIDI* | *TFDD* | *TREND* | *LABPTA* | *TOTA* | *GPTAD* |
-#' |:-----------------|:---------|:--------|:--------|:---------|:-------|:-------|
+#' |:-----------------|:---------|:--------|:-------|:--------|:---------|:-------|:--------|
 #' | Title  | Treaty Name | Name.of.the.agreement | DocumentName| Trade.Agreement | Name | name | Common.Name |
 #' | Signature  | Signature Date | signature.date | DateSigned | Year | year | date_signed | Date.of.Signature |
 #' | Force  | Date IEA entered into force | | | Year | year | date_into_force | Date.of.Entry.into.Force | 
 #' | mitchID  | IEA# | | | | | |
 #' | heidiID  | | ID | | | | |
-#' | labptaID | | | | | Number | | |
-#' | tfddID | | | 2016Update ID | | | | |
-#' | AgreementType  | Agreement Type (level 2) | | DocType | | | | |
+#' | labptaID | | | | | Number | |
+#' | tfddID | | | 2016Update ID | | | |
+#' | AgreementType  | Agreement Type (level 2) | | DocType | | | |
 #' | DocType  | Inclusion | | NumberParties | | | | Type |
-#' | GeogArea | | | GeoScope | | | | |
-#' | Basin | | | Basin Name | | | | |
-#' | Issue | | | Issue Area | | | | |
-#' | Lineage | | | PrimaryTFDDID | | | | |
+#' | GeogArea | | | GeoScope | | | |
+#' | Basin | | | Basin Name | | | |
+#' | Issue | | | Issue Area | | | |
+#' | Lineage | | | PrimaryTFDDID | | | |
 #' 
 #' @md
 #' @details
