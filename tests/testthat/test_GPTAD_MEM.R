91b9eba850127386ad3b9a4b9fd50ac7d480e424
Jael Tan <jael.tan@graduateinstitute.ch>
2025-02-25 12:22:47 +0100

    Updated tests to account for name changes in parties datacube

--- tests/testthat/test_GPTAD_MEM.R
+++ tests/testthat/test_GPTAD_MEM.R
@@ -2,49 +2,49 @@
 
 # Report missing values
 test_that("missing observations are reported correctly", {
-  expect_false(any(grepl("^n/a$", memberships[["GPTAD_MEM"]])))
-  expect_false(any(grepl("^N/A$", memberships[["GPTAD_MEM"]])))
-  expect_false(any(grepl("^\\s$", memberships[["GPTAD_MEM"]])))
-  expect_false(any(grepl("^\\.$", memberships[["GPTAD_MEM"]])))
-  expect_false(any(grepl("N\\.A\\.$", memberships[["GPTAD_MEM"]])))
-  expect_false(any(grepl("n\\.a\\.$", memberships[["GPTAD_MEM"]])))
+  expect_false(any(grepl("^n/a$", parties[["GPTAD"]])))
+  expect_false(any(grepl("^N/A$", parties[["GPTAD"]])))
+  expect_false(any(grepl("^\\s$", parties[["GPTAD"]])))
+  expect_false(any(grepl("^\\.$", parties[["GPTAD"]])))
+  expect_false(any(grepl("N\\.A\\.$", parties[["GPTAD"]])))
+  expect_false(any(grepl("n\\.a\\.$", parties[["GPTAD"]])))
 })
 
 # Uniformity tests (agreements have a stateID and Begin columns)
 test_that("datasets have the required variables", {
-  pointblank::expect_col_exists(memberships[["GPTAD_MEM"]],
+  pointblank::expect_col_exists(parties[["GPTAD"]],
                                 pointblank::vars(stateID))
-  pointblank::expect_col_exists(memberships[["GPTAD_MEM"]],
+  pointblank::expect_col_exists(parties[["GPTAD"]],
                                 pointblank::vars(Begin))
 })
 
 # Date columns should be in mdate class
 test_that("Columns are not in date, POSIXct or POSIXlt class", {
-  expect_false(any(lubridate::is.Date(memberships[["GPTAD_MEM"]])))
-  expect_false(any(lubridate::is.POSIXct(memberships[["GPTAD_MEM"]])))
-  expect_false(any(lubridate::is.POSIXlt(memberships[["GPTAD_MEM"]])))
+  expect_false(any(lubridate::is.Date(parties[["GPTAD"]])))
+  expect_false(any(lubridate::is.POSIXct(parties[["GPTAD"]])))
+  expect_false(any(lubridate::is.POSIXlt(parties[["GPTAD"]])))
 })
 
 # Dates are standardized for mandatory column
 test_that("Column `Begin` has standardised dates", {
-  expect_equal(class(memberships[["GPTAD_MEM"]]$Begin), "mdate")
-  expect_false(any(grepl("/", memberships[["GPTAD_MEM"]]$Begin)))
+  expect_equal(class(parties[["GPTAD"]]$Begin), "mdate")
+  expect_false(any(grepl("/", parties[["GPTAD"]]$Begin)))
   expect_false(any(grepl("^[:alpha:]$",
-                         memberships[["GPTAD_MEM"]]$Begin)))
+                         parties[["GPTAD"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{2}$",
-                         memberships[["GPTAD_MEM"]]$Begin)))
+                         parties[["GPTAD"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{3}$",
-                         memberships[["GPTAD_MEM"]]$Begin)))
+                         parties[["GPTAD"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{1}$",
-                         memberships[["GPTAD_MEM"]]$Begin)))
+                         parties[["GPTAD"]]$Begin)))
 })
 
 # Dataset should be ordered according to the "Begin" column
 test_that("dataset is arranged by the `Begin` variable", {
-  expect_true(memberships[["GPTAD_MEM"]]$Begin[1] <
-                memberships[["GPTAD_MEM"]]$Begin[100])
-  expect_true(memberships[["GPTAD_MEM"]]$Begin[120] <
-                memberships[["GPTAD_MEM"]]$Begin[220])
-  expect_true(memberships[["GPTAD_MEM"]]$Begin[250] <
-                memberships[["GPTAD_MEM"]]$Begin[350])
+  expect_true(parties[["GPTAD"]]$Begin[1] <
+                parties[["GPTAD"]]$Begin[100])
+  expect_true(parties[["GPTAD"]]$Begin[120] <
+                parties[["GPTAD"]]$Begin[220])
+  expect_true(parties[["GPTAD"]]$Begin[250] <
+                parties[["GPTAD"]]$Begin[350])
 })
--- tests/testthat/test_HUGGO_MEM.R
+++ tests/testthat/test_HUGGO_MEM.R
@@ -2,49 +2,49 @@
 
 # Report missing values
 test_that("missing observations are reported correctly", {
-  expect_false(any(grepl("^n/a$", memberships[["HUGGO_MEM"]])))
-  expect_false(any(grepl("^N/A$", memberships[["HUGGO_MEM"]])))
-  expect_false(any(grepl("^\\s$", memberships[["HUGGO_MEM"]])))
-  expect_false(any(grepl("^\\.$", memberships[["HUGGO_MEM"]])))
-  expect_false(any(grepl("N\\.A\\.$", memberships[["HUGGO_MEM"]])))
-  expect_false(any(grepl("n\\.a\\.$", memberships[["HUGGO_MEM"]])))
+  expect_false(any(grepl("^n/a$", parties[["HUGGO"]])))
+  expect_false(any(grepl("^N/A$", parties[["HUGGO"]])))
+  expect_false(any(grepl("^\\s$", parties[["HUGGO"]])))
+  expect_false(any(grepl("^\\.$", parties[["HUGGO"]])))
+  expect_false(any(grepl("N\\.A\\.$", parties[["HUGGO"]])))
+  expect_false(any(grepl("n\\.a\\.$", parties[["HUGGO"]])))
 })
 
 # Uniformity tests (agreements have a stateID and Begin columns)
 test_that("datasets have the required variables", {
-  pointblank::expect_col_exists(memberships[["HUGGO_MEM"]],
+  pointblank::expect_col_exists(parties[["HUGGO"]],
                                 pointblank::vars(stateID))
-  pointblank::expect_col_exists(memberships[["HUGGO_MEM"]],
+  pointblank::expect_col_exists(parties[["HUGGO"]],
                                 pointblank::vars(Begin))
 })
 
 # Date columns should be in mdate class
 test_that("Columns are not in date, POSIXct or POSIXlt class", {
-  expect_false(any(lubridate::is.Date(memberships[["HUGGO_MEM"]])))
-  expect_false(any(lubridate::is.POSIXct(memberships[["HUGGO_MEM"]])))
-  expect_false(any(lubridate::is.POSIXlt(memberships[["HUGGO_MEM"]])))
+  expect_false(any(lubridate::is.Date(parties[["HUGGO"]])))
+  expect_false(any(lubridate::is.POSIXct(parties[["HUGGO"]])))
+  expect_false(any(lubridate::is.POSIXlt(parties[["HUGGO"]])))
 })
 
 # Dates are standardized for mandatory column
 test_that("Column `Begin` has standardised dates", {
-  expect_equal(class(memberships[["HUGGO_MEM"]]$Begin), "mdate")
-  expect_false(any(grepl("/", memberships[["HUGGO_MEM"]]$Begin)))
+  expect_equal(class(parties[["HUGGO"]]$Begin), "mdate")
+  expect_false(any(grepl("/", parties[["HUGGO"]]$Begin)))
   expect_false(any(grepl("^[:alpha:]$",
-                         memberships[["HUGGO_MEM"]]$Begin)))
+                         parties[["HUGGO"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{2}$",
-                         memberships[["HUGGO_MEM"]]$Begin)))
+                         parties[["HUGGO"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{3}$",
-                         memberships[["HUGGO_MEM"]]$Begin)))
+                         parties[["HUGGO"]]$Begin)))
   expect_false(any(grepl("^[:digit:]{1}$",
-                         memberships[["HUGGO_MEM"]]$Begin)))
+                         parties[["HUGGO"]]$Begin)))
 })
 
 # Dataset should be ordered according to the "Begin" column
 test_that("dataset is arranged by the `Begin` variable", {
-  expect_true(memberships[["HUGGO_MEM"]]$Begin[1] <
-                memberships[["HUGGO_MEM"]]$Begin[100])
-  expect_true(memberships[["HUGGO_MEM"]]$Begin[120] <
-                memberships[["HUGGO_MEM"]]$Begin[220])
-  expect_true(memberships[["HUGGO_MEM"]]$Begin[250] <
-                memberships[["HUGGO_MEM"]]$Begin[350])
+  expect_true(parties[["HUGGO"]]$Begin[1] <
+                parties[["HUGGO"]]$Begin[100])
+  expect_true(parties[["HUGGO"]]$Begin[120] <
+                parties[["HUGGO"]]$Begin[220])
+  expect_true(parties[["HUGGO"]]$Begin[250] <
+                parties[["HUGGO"]]$Begin[350])
 })
