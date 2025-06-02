2adf06d7f180ebaf0cb7b4c447f578c15ef9c1c1
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-02 20:08:20 +0200

    Added code_term_date()

--- R/code_termination.R
+++ R/code_termination.R
@@ -79,3 +79,43 @@ code_term <- function(title, text = NULL) {
   type
 }
 
+#' Code treaty termination date
+#'
+#' Creates a column with the date of termination
+#' of the treaty.
+#' @param title title column variable.
+#' @param text treaty text column variable
+#' @return a character vector with the term date of the
+#' treaty
+#' @importFrom stringr str_remove_all str_extract_all
+#' @examples
+#' \dontrun{
+#' HUGGO <- dplyr::slice_sample(manyenviron::agreements$HUGGO, n = 200)
+#' code_term_date(HUGGO$Title)
+#' }
+#' @export
+code_term_date <- function(title, text = NULL) {
+  if (is.null(text)) {
+  # Step one: extract term date if present in treaty title
+  title <- as.character(title)
+  # Treaties with the term date in the title had the word "For the Period"
+  date <- ifelse(grepl("For The Period", title),
+                 stringr::str_extract_all(title, "\\d{1,2}.\\w{3,}.\\d{4}"), NA)
+  # Extract only the term date
+  date <- suppressWarnings(stringr::str_remove_all(date, "^c|\\(|\\)|\""))
+  date <- stringr::str_extract_all(date, "\\d{1,2}.\\w{3,}.\\d{4}$")
+  # Standardise the date format
+  date <- as.Date(as.character(date), format = "%d %B %Y")
+  } else {
+    # repeat same operation for the titles
+    title <- as.character(title)
+    date <- ifelse(grepl("For The Period", title),
+                   stringr::str_extract_all(title, "\\d{1,2}.\\w{3,}.\\d{4}"),
+                   NA)
+    date <- suppressWarnings(stringr::str_remove_all(date, "^c|\\(|\\)|\""))
+    date <- stringr::str_extract_all(date, "\\d{1,2}.\\w{3,}.\\d{4}$")
+    date <- as.Date(as.character(date), format = "%d %B %Y")
+  }
+  date
+}
+
