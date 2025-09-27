84e5f553b3f53df094b78903395c8f918281dbf4
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-27 23:22:13 +0200

    Updated TFDD dataset with proper missing processing, new variable names, dropped unnecessary or complicated variables, but retained more

--- data-raw/agreements/TFDD/prepare-TFDD.R
+++ data-raw/agreements/TFDD/prepare-TFDD.R
@@ -4,7 +4,8 @@
 # ready for the many package.
 
 # Stage one: Collecting data
-TFDD <- readr::read_csv("data-raw/agreements/TFDD/TFDD.csv")
+TFDD <- readr::read_csv("data-raw/agreements/TFDD/TFDD.csv",
+                        na = c("", "NA", "N/A", "#N/A","N.A."))
 
 # Stage two: Correcting data
 # In this stage you will want to correct the variable names and
@@ -13,36 +14,47 @@ TFDD <- readr::read_csv("data-raw/agreements/TFDD/TFDD.csv")
 # We recommend that you avoid using one letter variable names to keep
 # away from issues with ambiguous names down the road.
 TFDD <- as_tibble(TFDD) %>%
+  dplyr::select(-matches("2010|_Article|_Text|TFDDID|Preced|Construct_")) %>%
+  dplyr::select(-c(`Entry ID`, BCCODE, BCODE, CCODE, Country_Name, Signatories,
+                   `Unified Primary 2016 Treaty ID`,`DocID Linkage`,RBO,
+                   Region_Continent,`Treaty Basin(s)`,`TFDD Basin(s)`,MinorAgreement,
+                   Allocation,Environment,Format,NeedsVariability,AltScenarios,
+                   `Conflict Resolution Mechanism`,NonSig_Riparians,VarMgmt,
+                   StakeholderParticip,Compensation,`Uncertainty Explicit`,
+                   PriorNotification,`NonWater Linkage`,EnforceMech,Enforcement,
+                   WaterDiv_Method,`Agreement Purpose`,GeoScope)) %>%
   manydata::transmutate(Signature = messydates::as_messydate(DateSigned),
                         Begin = messydates::as_messydate(as.character(Signature)),
-                        tfddID = `2016Update ID`,
+                        tfddID = as.character(`2016Update ID`),
                         Title = manytreaties::standardise_titles(DocumentName),
-                        Lineage = PrimaryTFDDID, # original agreement to which a replacement, amendment or protocol refers
-                        Basin = `Basin Name`,
-                        Issue = `Issue Area`,
-                        Comments = Notes) %>%
-  dplyr::mutate(AgreementType = dplyr::case_match(DocType,
-                  c(2, 4, 8) ~ "A",
-                  6 ~ "E",
-                  5 ~ "S", # Superseded
-                  7 ~ "P",
-                  .default = NA),
-                NotTreaty = ifelse(DocType == 1, 1, 0),
-                Water_as_consumable_rsc = ifelse(DocType == 3, 0, 1),
-                GeogArea = dplyr::case_match(GeoScope,
-                  1 ~ "G",
-                  2 ~ "R",
-                  3 ~ "SR", # Subregional
-                  4 ~ "B", # Basin
-                  5 ~ "SB", # Sub-basin
-                  .default = NA),
-                DocType = dplyr::case_match(NumberParties,
-                  1 ~ "B",
-                  2 ~ "M"
-                )) %>%
-  dplyr::select(Title, Begin, Signature, AgreementType, DocType, GeogArea,
-                Water_as_consumable_rsc, NotTreaty, Issue, Basin,
-                Lineage, tfddID) %>%
+                        # TypeAmbit = dplyr::case_match(GeoScope,
+                        #                               1 ~ "Global",
+                        #                               2 ~ "Regional",
+                        #                               3 ~ "Subregional", # Subregional
+                        #                               4 ~ "Basin", # Basin
+                        #                               5 ~ "Subbasin", # Sub-basin
+                        #                               .default = NA),
+                        TypeAgree = dplyr::case_match(DocType,
+                                                      c(2, 4, 8) ~ "Agreement",
+                                                      6 ~ "Amendment",
+                                                      # 5 ~ "S", # Superseded
+                                                      7 ~ "Protocol",
+                                                      .default = NA),
+                        TypeAmbit = dplyr::case_match(NumberParties,
+                                                      1 ~ "Bilateral",
+                                                      2 ~ "Multilateral"
+                        ),
+                        # Lineage = PrimaryTFDDID, # original agreement to which a replacement, amendment or protocol refers
+                        TypeRegion = PRIMARY_COUNTRY_SUBREGION,
+                        TypeGeo = dplyr::coalesce(`Basin Name`,Basin_Name),
+                        TypeSubject = `Issue Area`,
+                        Secretariat = CommName,
+                        Location = LocationSigned,
+                        Comments = dplyr::coalesce(Notes, AdditionalComments,NonWaterLink_Comments)) %>%
+  dplyr::select(Title, Begin, Signature, 
+                TypeAgree, TypeAmbit, TypeRegion, TypeGeo, TypeSubject,
+                Location, Secretariat, Comments, Source, tfddID, everything()) %>%
+  dplyr::filter(!is.na(Title)) %>%
   dplyr::distinct() %>%
   dplyr::arrange(Begin)
 
@@ -59,40 +71,11 @@ TFDD$treatyID <- manytreaties::code_agreements(TFDD,
 TFDD <- dplyr::relocate(TFDD, treatyID)
 
 # Remove duplicates and ensure NAs are coded correctly
-TFDD <- TFDD %>%
-  dplyr::filter(!is.na(Title)) %>%
-  dplyr::mutate(Issue = dplyr::case_match(Issue,
-                                          c("0", "#N/A", "Y") ~ NA,
-                                          .default = Issue),
-                across(everything(),
-                       ~stringr::str_replace_all(.,
-                                                 "^NA$", NA_character_))) %>%
-  dplyr::mutate(Begin = messydates::as_messydate(Begin),
-                Signature = messydates::as_messydate(Signature)) %>%
-  dplyr::distinct(.keep_all = TRUE)
-
-# manypkgs includes several functions that should help with
-# cleaning and standardising your data
-# such as `standardise_titles()` and `standardise_texts()`.
-# Please see the vignettes or website for more details.
+# TFDD <- TFDD %>%
+#   dplyr::mutate(Begin = messydates::as_messydate(Begin),
+#                 Signature = messydates::as_messydate(Signature)) %>%
+#   dplyr::distinct(.keep_all = TRUE)
 
 # Stage three: Connecting data
-# Next run the following line to make TFDD available
-# within the package.
-# This function also does two additional things.
-# First, it creates a set of tests for this object to ensure adherence
-# to certain standards.
-# You can hit Cmd-Shift-T (Mac) or Ctrl-Shift-T (Windows)
-# to run these tests locally at any point.
-# Any test failures should be pretty self-explanatory
-# and may require you to return to stage two and further clean,
-# standardise, or wrangle your data into the expected format.
-# Second, it also creates a documentation file for you to fill in.
-# Please note that the export_data() function requires a .bib file to be
-# present in the data_raw folder of the package for citation purposes.
-# Therefore, please make sure that you have permission to use the dataset
-# that you're including in the package.
-# To add a template of .bib file to the package,
-# please run `manypkgs::add_bib("agreements", "TFDD")`.
 manypkgs::export_data(TFDD, datacube = "agreements",
                       URL = "https://transboundarywaters.science.oregonstate.edu/")
